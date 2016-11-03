343b62e60cd6ce5436db30e09c0c0b77ae6ac2b7
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-11-04 00:22:13 +0300

    "ducers"

--- library/Router/ParamsParser.hs
+++ library/Router/ParamsParser.hs
@@ -3,27 +3,35 @@ module Router.ParamsParser where
 import Router.Prelude
 import qualified ByteString.TreeBuilder as C
 import qualified Router.HTTPAuthorizationParser as D
+import qualified Ducers.Reducer as E
 
 
 newtype Params a =
-  Params (IO ByteString -> IO (Either Text a))
+  Params (E.Reducer IO ByteString (Either Text a))
   deriving (Functor)
 
 instance Applicative Params where
   pure x =
-    Params (const (pure (pure x)))
+    Params (pure (pure x))
   (<*>) (Params impl1) (Params impl2) =
     undefined
 
 instance Alternative Params where
 
 param :: ByteString -> Param a -> Params a
-param =
+param name (Param paramReducer) =
+  Params (paramsReducer)
+  where
+    paramsReducer =
+      undefined
+
+paramByText :: Text -> Param a -> Params a
+paramByText =
   undefined
 
 
 newtype Param a =
-  Param (IO ByteString -> IO (Either Text a))
+  Param (E.Reducer IO ByteString (Either Text a))
   deriving (Functor)
 
 text :: Param Text
