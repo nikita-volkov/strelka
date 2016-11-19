1802d27e969391f186528140a59e597769a10a41
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-11-19 03:08:21 +0300

    Move the execution logic to Route

--- library/Router/Route.hs
+++ library/Router/Route.hs
@@ -5,10 +5,24 @@ import Router.Model
 import qualified Router.RequestHeadParser as A
 import qualified Router.RequestBodyConsumer as B
 import qualified Router.ResponseBuilder as C
+import qualified Data.Text as F
+import qualified Data.Text.Encoding as D
+import qualified Data.Text.Encoding.Error as E
 
 
 type Route m =
   A.RequestHeadParser m (B.RequestBodyConsumer (m C.ResponseBuilder))
 
-
-
+run :: MonadIO m => Route m -> Request -> m (Either Text Response)
+run requestHeadParser request =
+  do
+    requestHeadParserResult <- A.run requestHeadParser request segments
+    forM requestHeadParserResult $ \(B.RequestBodyConsumer consumeBody, _) -> do
+      getResponseBuilder <- liftIO (consumeBody consumeBodyChunk)
+      responseBuilder <- getResponseBuilder
+      return (C.run responseBuilder)
+  where
+    Request _ (Path pathBytes) _ _ (InputStream consumeBodyChunk) =
+      request
+    segments =
+      filter (not . F.null) (F.splitOn "/" (D.decodeUtf8With E.lenientDecode pathBytes))
