2bbef76b8d246aa8e288d6fdf50881d03074f68a
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-02-01 11:36:50 +0300

    Move the multi-value logic to Value

--- library/Strelka/ParamsParser.hs
+++ library/Strelka/ParamsParser.hs
@@ -3,46 +3,54 @@ DSL for parsing of parameters.
 -}
 module Strelka.ParamsParser where
 
-import Strelka.Prelude
+import Strelka.Prelude hiding (maybe, list)
+import qualified Strelka.Prelude as A
 import qualified Strelka.HTTPAuthorizationParser as D
 import qualified Data.Attoparsec.Text as G
 
 
 newtype Params a =
   Params (ReaderT (Text -> Maybe [Text]) (Except Text) a)
-  deriving (Functor, Applicative, Alternative, Monad, MonadPlus, MonadError Text)
+  deriving (Functor, Applicative, Alternative)
 
 value :: Text -> Value a -> Params a
 value name value =
-  valueMaybe name value >>= \case
-    Just x -> return x
-    Nothing -> throwError ("No value specified for the parameter \"" <> name <> "\"")
-
-valueList :: Text -> Value a -> Params [a]
-valueList name value =
-  Strelka.ParamsParser.lookup name >>= parseValues value
+  Params (ReaderT onLookup)
   where
-    parseValues (Value parser) inputs =
-      Params (lift (traverse (except . either (Left . updateError) Right . runExcept . runReaderT parser) inputs))
+    onLookup lookup =
+      A.maybe notFound found (lookup name)
       where
-        updateError x =
-          "Parameter \"" <> name <> "\" parsing failure: " <> x
+        notFound =
+          (except . Left) ("Parameter \"" <> name <> "\" not found")
+        found input =
+          case value of
+            Value (ReaderT onInput) -> withExcept updateError (onInput input)
+          where
+            updateError x =
+              "Parameter \"" <> name <> "\" values parsing failure: " <> x
 
-valueMaybe :: Text -> Value a -> Params (Maybe a)
-valueMaybe name =
-  fmap listToMaybe . valueList name
+lenientParams1 :: LenientValue a => Text -> Params a
+lenientParams1 name1 =
+  value name1 lenientValue
 
-lookup :: Text -> Params [Text]
-lookup name =
-  Params (ReaderT (\lookup -> maybe notFound return (lookup name)))
-  where
-    notFound =
-      throwError ("Parameter \"" <> name <> "\" not found")
+lenientParams2 :: (LenientValue a, LenientValue b) => Text -> Text -> Params (a, b)
+lenientParams2 name1 name2 =
+  (,) <$>
+  lenientParams1 name1 <*>
+  lenientParams1 name2
+
+lenientParams3 :: (LenientValue a, LenientValue b, LenientValue c) => Text -> Text -> Text -> Params (a, b, c)
+lenientParams3 name1 name2 name3 =
+  (,,) <$>
+  lenientParams1 name1 <*>
+  lenientParams1 name2 <*>
+  lenientParams1 name3
+
 
 
 newtype Value a =
-  Value (ReaderT Text (Except Text) a)
-  deriving (Functor, Applicative, Alternative, Monad, MonadPlus, MonadError Text)
+  Value (ReaderT [Text] (Except Text) a)
+  deriving (Functor, Applicative, Alternative)
 
 parser :: G.Parser a -> Value a
 parser parser =
@@ -53,20 +61,41 @@ parser parser =
 
 matcher :: (Text -> Either Text a) -> Value a
 matcher matcher =
-  Value (ReaderT (except . matcher))
+  Value (ReaderT (except . join . liftM matcher . head))
+  where
+    head =
+      \case
+        x : _ -> Right x
+        _ -> Left ("Not a single value is specified")
 
 text :: Value Text
 text =
-  Value (ReaderT pure)
+  matcher Right
 
 char :: Value Char
 char =
   parser G.anyChar
 
+list :: Value a -> Value [a]
+list (Value (ReaderT singleValueFn)) =
+  Value (ReaderT (traverse (singleValueFn . pure)))
+
+maybe :: Value a -> Value (Maybe a)
+maybe (Value (ReaderT singleValueFn)) =
+  Value (ReaderT (traverse (singleValueFn . pure) . listToMaybe))
+
 
 
 class LenientValue value where
   lenientValue :: Value value
+
+instance LenientValue a => LenientValue (Maybe a) where
+  lenientValue =
+    maybe lenientValue
+
+instance LenientValue a => LenientValue [a] where
+  lenientValue =
+    list lenientValue
 
 instance LenientValue Text where
   lenientValue =
@@ -76,17 +105,3 @@ instance LenientValue Char where
   lenientValue =
     char
 
-
-lenientParams1 :: LenientValue a => Text -> Params a
-lenientParams1 name1 =
-  value name1 lenientValue
-
-lenientParams2 :: (LenientValue a, LenientValue b) => Text -> Text -> Params (a, b)
-lenientParams2 name1 name2 =
-  (,) <$> lenientParams1 name1 <*> lenientParams1 name2
-
-lenientParams3 :: (LenientValue a, LenientValue b, LenientValue c) => Text -> Text -> Text -> Params (a, b, c)
-lenientParams3 name1 name2 name3 =
-  (,,) <$> lenientParams1 name1 <*> lenientParams1 name2 <*> lenientParams1 name3
-
-  
